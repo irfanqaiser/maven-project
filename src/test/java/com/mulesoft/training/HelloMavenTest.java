b0c5c06ade540c19d3846ba2066e1adc12e2f675
irfan <irfan_qaiser@newflyer.com>
2018-04-04 12:38:20 -0500

    Added CloudHub deploy plugin

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -3,10 +3,12 @@ package com.mulesoft.training;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 //import org.junit.runner.RunWith;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.junit.Rule;
 import org.mule.tck.junit4.rule.DynamicPort;
+
 public class HelloMavenTest extends FunctionalTestCase {
 	
 	@Rule
@@ -16,7 +18,13 @@ public class HelloMavenTest extends FunctionalTestCase {
     	System.out.println("\n\nIn Testcase-1 Http port used ---------> " + myPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
-    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	System.out.println("\n\nIn Testcase-1 Http port used ---------> " + myPort.getNumber() + "\n\n");
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }    
     @Override
     protected String[] getConfigFiles() {
         String[] files = {"maven-project.xml", "global.xml"};
