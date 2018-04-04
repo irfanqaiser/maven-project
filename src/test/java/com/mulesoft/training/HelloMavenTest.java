4103a6d0d55ee2b17c76bcf6ea7070ee01382802
irfan <irfan_qaiser@newflyer.com>
2018-04-04 11:49:49 -0500

    Formatted all XML for code quality

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -3,7 +3,7 @@ package com.mulesoft.training;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.junit.Rule;
 import org.mule.tck.junit4.rule.DynamicPort;
@@ -13,13 +13,14 @@ public class HelloMavenTest extends FunctionalTestCase {
 	public DynamicPort myPort = new DynamicPort("http.port");
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
-    	System.out.println("\n\nIn Testcase-1 Http port user ---------> " + myPort.getNumber() + "\n\n");
+    	System.out.println("\n\nIn Testcase-1 Http port used ---------> " + myPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
     @Override
-    protected String getConfigFile() {
-        return "maven-project.xml";
+    protected String[] getConfigFiles() {
+        String[] files = {"maven-project.xml", "global.xml"};
+        return files;
     }
 
 }
